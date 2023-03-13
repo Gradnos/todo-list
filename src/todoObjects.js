c6ee013481f99234c2b55df99e3bc8cdd47a8027
Gradnos <sdolaqidze@gmail.com>
2023-03-13 18:03:36 +0400

    Create a simple User factory

--- src/todoObjects.js
+++ src/todoObjects.js
@@ -9,7 +9,7 @@ export const todoItemFactory = (title, description, dueDate, priority) =>{
 
 
 export const projectFactory = (title) =>{
-    let todoArr = []
+    let todoArr = [];
 
     const addTodo = (todoItem) =>{
         todoArr.push(todoItem);
@@ -22,7 +22,7 @@ export const projectFactory = (title) =>{
     }   
 
     const getTodoId = (todoItem) =>{
-        return todoArr.findIndex(element => element === todoItem);
+        return todoArr.findIndex(element => element.title === todoItem.title);
     }
 
     return{
@@ -32,3 +32,29 @@ export const projectFactory = (title) =>{
         removeTodo
     }
 };
+
+export const User = (username) =>{
+    let projectArr =[];
+
+    const addProject = (projectItem) =>{
+        projectArr.push(projectItem);
+    }
+
+    const removeProject = (projectItem) =>{
+        let id = getTodoId(projectItem);
+        if(id === undefined) return;
+        projectArr.splice(id, 1);
+    }   
+
+    const getTodoId = (projectItem) =>{
+        return projectArr.findIndex(element => element.title === projectItem.title);
+    }
+
+    return{
+        username,
+        addProject,
+        removeProject
+    };
+}
+
+
