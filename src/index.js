3225161585e85b3efb91a147c134c8069a641027
Gradnos <sdolaqidze@gmail.com>
2023-03-15 18:32:18 +0400

    Update project template +Add a favicon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './style.css';
 import { todoItemFactory, projectFactory, userFactory } from './todoObjects';
+import Icon from "./icons/favicon.svg";
+
+let link = document.createElement('link');
+link.rel = 'icon';
+link.href = Icon;
+document.head.appendChild(link);
 
 
 
